daa02e8e3b90b8de4a3be22c2ea1f21c3bafb6aa
judplaza <58659463+judplaza@users.noreply.github.com>
2021-12-10 11:43:03 +0100

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -2,17 +2,34 @@
         require_once '../dbconfig.php';
         parse_str($_SERVER['QUERY_STRING'],$variables);
 
-        $query = "SELECT `date`,`subject`,`name` FROM `tasks` WHERE `student_id`='{$variables[student_id]}'";
-        if(count($variables)>0){
+        $query = "SELECT `date`,`subject`,`name` FROM `tasks` WHERE `student_id`='{$variables[>        if(count($variables)>0){
                 foreach($variables as $key => $val){
                         if($key !="limit" AND $key!="student_id"){
                                 if(!is_array($val)){
                                         $query.=" AND `{$key}`='{$val}'";
                                 }else{
-                                        if($val[gte]!="now"){
-                                                $query.=" AND `date`>='{$val[gte]}'";
+                                        $k=array_keys($val);
+                                        switch($k[0]){
+                                                case "gt":
+                                                $query.=" AND `date`>";
+                                                break;
+
+                                                case "gte":
+                                                $query.=" AND `date`>=";
+                                                break;
+
+                                                case "lt":
+                                                $query.=" AND `date`<";
+                                                break;
+
+                                                case "lte":
+                                                $query.=" AND `date`<=";
+                                                break;
+                                        }
+                                        if($val[$k[0]]!="now"){
+                                                $query.="'{$val[$k[0]]}'";
                                         }else{
-                                                $query.=" AND `date`>=CURRENT_DATE";
+                                                $query.="CURRENT_DATE";
                                         }
                                 }
                         }
@@ -22,11 +39,11 @@
         if(array_key_exists("limit",$variables)){
                 $query.= " LIMIT {$variables[limit]}";
         }
-        $result = mysqli_query($connection,$query) or die("Error in Selecting " .mysqli_error($connection));
-        $emparray = array();
+        $result = mysqli_query($connection,$query) or die("Error in Selecting " .mysqli_error(>     $emparray = array();
         while($row = mysqli_fetch_assoc($result)){
                 $emparray[] = $row;
         }
         echo json_encode(['tasks'=>$emparray]);
         mysqli_close($connection);
 ?>
+
